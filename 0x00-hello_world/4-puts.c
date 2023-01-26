ea636bad524980c663359dbc4e809d7a49e7ce31
root <homesbyrhoades@gmail.com>
2023-01-26 10:58:43 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints quote
+ *
+ * Return: zero on success
+ *
+ */
+int main(voud)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
