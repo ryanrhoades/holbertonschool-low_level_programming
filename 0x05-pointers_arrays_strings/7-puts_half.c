4267e3622081475c9a1e06fd7b2cc6d9d1aee2e4
root <homesbyrhoades@gmail.com>
2023-02-06 11:30:09 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,16 +16,16 @@ void puts_half(char *str)
 	}
 	if (length % 2 != 0)
 	{
-		n = ((length + 1) / 2;
+		n = ((length + 1) / 2);
 	}
 	else
 	{
-		n - (length / 2);
+		n = (length / 2);
 	}
 	while (str[n] != '\0')
 	{
 		_putchar(str[n]);
-	n++;
+		n++;
 	}
-	_putchar('\n'n);
+	_putchar('\n');
 }
