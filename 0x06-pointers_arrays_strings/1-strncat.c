938a8ed38c80d6204349d1ed98c70960098bd848
root <homesbyrhoades@gmail.com>
2023-02-11 14:53:48 -0800

    -strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,17 +1,27 @@
 #include "holberton.h"
 
 /**
- * swap_int - function that swaps the values of two integers.
- * @a: this is the first entry
- * @b: this is the second entry
- *
- *
+ * _strncat - adds a string to a string
+ * @dest: destination for the final string
+ * @src: source string
+ * @n: number of bytes from src to be placed in dest
+ * Return: compounded string inside dest
  */
-void swap_int(int *a, int *b)
-{
-	int changed;
 
-	changed = *a;
-	*a = *b;
-	*b = changed;
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0;
+	int dl = 0;
+
+	while (dest[dl])
+	{
+		dl++;
+	}
+	for (index = 0; src[index] && index < n; index++)
+	{
+		dest[dl] = src[index];
+		dl++;
+	}
+
+	return (dest);
 }
