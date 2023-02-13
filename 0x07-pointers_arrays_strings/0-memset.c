daf7ab7ae3a45735402bb35fd521ac7176ed7788
root <homesbyrhoades@gmail.com>
2023-02-13 11:21:39 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,25 @@
 #include "holberton.h"
 
 /**
- * *_memset - Fills memory with a constant byte
+ * _memset - changes amount n bytes to byte b
  *
- * @s: pointer
- * @b: char
- * @n: unsigned int
+ * @n: number of bytes to be changed
+ * @s: points to data being changed
+ * @b: byte being applied
  *
- *Return: s
- */
+ * Return: a pointer pointing to value being changed
+ **/
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
+	char **s2;
+	unsigned int a = 0;
 
-	for (index = 0; n > 0; index++)
+	s2 = &s;
+	while (a < n)
 	{
-		s[index] = b;
-		n -= 1;
+		s[a] = b;
+		a++;
 	}
-	return (s);
+	return (*s2);
 }
