786d64a24e1e53411adbeeb391c3eab92ef51c9c
root <homesbyrhoades@gmail.com>
2023-02-20 17:51:22 -0800

    3-strcmp.c git commit -m 3-strcmp.c

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares 2 strings
+ * @s1: container of string 1
+ * @s2: container of string 2
+ * Return: 0 if strings are the same
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
