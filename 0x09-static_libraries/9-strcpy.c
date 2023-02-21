4afac6b536c34931572337c7953c7916a518e378
root <homesbyrhoades@gmail.com>
2023-02-20 18:58:38 -0800

    9-strcpy.c

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strcpy I hope I'm doing this right
+ * @dest: don't make me do this again
+ * @src: Why is it still hard
+ * Return: with good grades
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (0);
+}
